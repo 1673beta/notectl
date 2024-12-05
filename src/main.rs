4574f67aa54782e9b3c546f5f21f449341ae5d47
Esurio <esurio@esurio1673.net>
2024-12-05 10:25:08 +0900

    feat: aid/aidxの時刻を表示するときにrfc3339形式で表示するように

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ mod util;
 
 #[tokio::main]
 async fn main() {
-    let id = util::id::aid::parse("a1dry0dc00");
-    println!("id: {:?}", id);
+    let id = util::id::aid::formatted_time("a1dry0dc00");
+    println!("id: {}", id);
 }
--- src/util/id/aidx.rs
+++ src/util/id/aidx.rs
@@ -1,10 +1,18 @@
 use std::time::{SystemTime, UNIX_EPOCH, Duration};
 use std::num::ParseIntError;
 
+use chrono::{DateTime, Local};
+
 const TIME2000: u64 = 946684800000;
 
 pub fn parse(id: &str) -> Result<SystemTime, ParseIntError> {
     let time_part = &id[0..8];
     let time = u64::from_str_radix(time_part, 36)? + TIME2000;
     Ok(UNIX_EPOCH + Duration::from_millis(time))
+}
+
+pub fn formatted_time(id: &str) -> String {
+    let systime = parse(id).unwrap();
+    let datetime: DateTime<Local> = systime.into();
+    datetime.to_rfc3339()
 }
