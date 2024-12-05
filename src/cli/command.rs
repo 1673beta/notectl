a1ad078cee06783d2473389203b37f28e93ec7da
Esurio <esurio@esurio1673.net>
2024-12-05 15:34:54 +0900

    add: meilisearch scope

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -1,1 +1,27 @@
-use clap::{Arg, Command, Subcommand};
+use clap::{Args, Parser, Subcommand};
+use crate::cli::vapid::generate;
+use crate::cli::config::show::ConfigCommand;
+
+#[derive(Debug, Parser)]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub cmd: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    Webpush(generate::VapidCommand),
+    Config(ConfigCommand)
+}
+
+pub async fn exec() {
+    let args = Cli::parse();
+    match args.cmd {
+        Commands::Webpush(cmd) => {
+            cmd.exec().unwrap();
+        },
+        Commands::Config(cmd) => {
+            cmd.exec().unwrap();
+        }
+    }
+}
