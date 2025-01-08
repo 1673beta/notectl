23eafca7b83b422c92ec8cf0f9a32f6cb5851d7a
Esurio <esurio@esurio1673.net>
2025-01-08 14:00:54 +0000

    feat: id gen command

---
+++ src/cli/id/gen.rs
@@ -0,0 +1,30 @@
+use crate::util;
+
+use super::IdType;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub fn gen(format: IdType) -> String {
+    let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64;
+    match format {
+        IdType::Aid => {
+            let id = util::id::aid::gen_aid(time).unwrap();
+            format!("generated aid: {}", id)
+        }
+        IdType::Aidx => {
+            let id = util::id::aidx::gen_aidx(time).unwrap();
+            format!("generated aidx: {}", id)
+        }
+        IdType::Meid => {
+            let id = util::id::meid::gen_meid(time);
+            format!("generated meid: {}", id)
+        }
+        IdType::ObjectId => {
+            let id = util::id::objectid::gen_object_id(time);
+            format!("generated objectid: {}", id)
+        }
+        IdType::Ulid => {
+            let id = util::id::ulid::gen_ulid(time);
+            format!("generated ulid: {}", id)
+        }
+    }
+}
--- src/cli/id/mod.rs
+++ src/cli/id/mod.rs
@@ -1,13 +1,24 @@
 pub mod parse;
+pub mod gen;
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use parse::parse;
+use gen::gen;
 
 #[derive(Debug, Parser)]
 #[command(name = "id")]
 pub struct IdCommand {
     #[command(subcommand)]
     pub subcmd: IdSubCommand,
+}
+
+#[derive(Debug, Clone, ValueEnum, Copy)]
+pub enum IdType {
+    Aid,
+    Aidx,
+    Meid,
+    ObjectId,
+    Ulid,
 }
 
 #[derive(Debug, Subcommand)]
@@ -18,6 +29,10 @@ pub enum IdSubCommand {
         #[arg(short = 'i', long = "id")]
         id: String,
     },
+    Gen {
+        #[arg(short = 'f', long = "format")]
+        id_type: IdType,
+    }
 }
 
 impl IdCommand {
@@ -25,6 +40,9 @@ impl IdCommand {
         match &self.subcmd {
             IdSubCommand::Parse { id_type, id } => {
                 println!("{}", parse(id, id_type))
+            }
+            IdSubCommand::Gen { id_type } => {
+                println!("{}", gen(*id_type))
             }
         }
     }
