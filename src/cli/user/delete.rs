bdd133134372ae1a2b336758d0732326784c3aa8
Esurio/1673beta <60435625+1673beta@users.noreply.github.com>
2025-02-18 02:10:01 +0900

    feat: `user delete`でusernameを指定できるように (#39)
    * feat: usernameをuser deleteで指定できるように
    
    * Update CHANGELOG

--- src/cli/user/delete.rs
+++ src/cli/user/delete.rs
@@ -5,7 +5,7 @@ use sea_orm::{ColumnTrait, EntityTrait, ModelTrait, QueryFilter, TransactionTrai
 use crate::db::postgres::connect_pg;
 use crate::entities::{prelude::*, user};
 
-pub async fn delete(config_path: &str, id: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn delete(config_path: &str, id: Option<Vec<&str>>, username: Option<Vec<&str>>) -> Result<(), Box<dyn std::error::Error>> {
   let pg_client = connect_pg(config_path).await?;
   let txn = pg_client.begin().await?;
   // need to output logs
@@ -13,12 +13,24 @@ pub async fn delete(config_path: &str, id: Vec<&str>) -> Result<(), Box<dyn std:
     .with_max_level(tracing::Level::INFO)
     .init();
 
-  let user_id = id.iter().map(|i| i.to_string()).collect::<Vec<String>>();
+  if id.is_none() && username.is_none() {
+    tracing::error!("Either username or id is required.");
+    std::process::exit(1);
+  }
 
-  let accounts = User::find()
-    .filter(user::Column::Id.is_in(user_id))
-    .all(&pg_client)
-    .await?;
+  let mut query = User::find();
+
+  if let Some(id) = id {
+    let user_id = id.iter().map(|i| i.to_string()).collect::<Vec<String>>();
+    query = query.filter(user::Column::Id.is_in(user_id));
+  }
+
+  if let Some(username) = username {
+    let user_name = username.iter().map(|u| u.to_string()).collect::<Vec<String>>();
+    query = query.filter(user::Column::Username.is_in(user_name));
+  }
+
+  let accounts = query.all(&txn).await?;
 
   for account in accounts {
     if account.host.is_none() {
--- src/cli/user/mod.rs
+++ src/cli/user/mod.rs
@@ -2,7 +2,7 @@ pub mod cull;
 pub mod delete;
 pub mod prune;
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ArgGroup};
 use cull::cull;
 use delete::delete;
 use prune::prune;
@@ -20,11 +20,20 @@ pub enum UserSubCommand {
     #[arg(short = 'c', long = "config", default_value = ".config/default.yml")]
     config_path: String,
   },
+  #[command(group(
+    ArgGroup::new("user_delete")
+      .required(true)
+      .args(&["id", "username"])
+  ),
+    about = "Delete user account(s)",
+    long_about = "Delete remote user account(s) by ID or username. Need specify either ID or username.")]
   Delete {
     #[arg(short = 'c', long = "config", default_value = ".config/default.yml")]
     config_path: String,
-    #[arg(short = 'i', long = "id", value_delimiter = ',')]
-    id: Vec<String>,
+    #[arg(short = 'i', long = "id", value_delimiter = ',', help = "Id of user you want to delete. You can specify multiple IDs with comma. e.g. -i a4ct2ps000,a3tghk0000")]
+    id: Option<Vec<String>>,
+    #[arg(short = 'u', long = "username", value_delimiter = ',', help = "Username of user you want to delete. You can specify multiple usernames with comma. e.g. -u alice,bob")]
+    username: Option<Vec<String>>,
   },
   Prune {
     #[arg(short = 'c', long = "config", default_value = ".config/default.yml")]
@@ -44,9 +53,14 @@ impl UserCommand {
       UserSubCommand::Cull { config_path } => {
         cull(config_path).await?;
       }
-      UserSubCommand::Delete { config_path, id } => {
-        let refs = id.iter().map(|s| s.as_str()).collect();
-        delete(config_path, refs).await?;
+      UserSubCommand::Delete { config_path, id, username } => {
+        let id_ref = id.as_ref().map(|v| {
+          v.iter().map(|s| s.as_str()).collect::<Vec<&str>>()
+        });
+        let username_ref = username.as_ref().map(|v| {
+          v.iter().map(|s| s.as_str()).collect::<Vec<&str>>()
+        });
+        delete(config_path, id_ref, username_ref).await?;
       }
       UserSubCommand::Prune {
         config_path,
